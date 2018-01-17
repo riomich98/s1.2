90d84fdafd3f9e45d6e97700f17d3fd47837af14
Rio Michelini <riomich98@gmail.com>
2018-01-17 17:15:15 +0000

    Added both buttons on FRDM board. Added isPressed function. Value of 0 for pressed button. Value of 1 for not pressed button. Due to the electrical characteristics of a circuit, positive voltage is seen as a logical 1, and negative voltage is seen as logical 0.

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,44 @@
 
 #include <stdbool.h>
 
+bool isPressed(DigitalIn button) {
+  if (button.read() == 1) {
+    // if the button is 1 and evaluates to true, the button is not pressed, and false is returned
+    return false;
+  } else {
+    // if the button is pressed, the true is returned
+    return true;
+  }
+}
+
 int main() {
-    bool flashing = false;
+    DigitalOut red(PTB22); // red LED
+    DigitalOut green(PTE26); // green LED
+    DigitalIn  btnRight(PTC6); // right hand button on board
+    DigitalIn  btnLeft(PTA4); // left hand button on board
 
-    DigitalOut red(PTB22);
-    DigitalIn  btn(PTC6);
-
-    while(1) {
-        if( btn.read() ) flashing = true;
-            else         flashing = false;
-
-        printf("button %d\n", btn.read() );
-        if(flashing){
-            red.write(0);
-            wait(0.5);
-            red.write(1);
-            wait(0.5);
-        }else{
-            red.write(1);
-            wait(1);
+    while(1) { // while true (always)
+      green.write(1); // turn LEDs off at first run
+      red.write(1);
+        if (isPressed(btnLeft) || isPressed(btnRight)) { // check that any buttons are actually pressed down
+        if(isPressed(btnLeft)) { // if left button is pressed down, using isPressed function
+            printf("Left Button Pressed"); // print status out to console
+            red.write(0); // turn on LED
+            wait(0.25);
+            red.write(1); // turn off LED
+            wait(0.25);
+        }
+        else if (isPressed(btnRight)) { // if right button is pressed down, using isPressed function
+            printf("Right Button Pressed"); // print status out to console
+            green.write(0); // turn on LED
+            wait(0.25);
+            green.write(1); // turn off LED
+            wait(0.25);
+        }
+        else { // turn off all LEDs
+          green.write(1);
+          red.write(1);
         }
     }
+  }
 }
